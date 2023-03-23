e31f71d9e5f730d344b866c3a1f90b828a0122cd
resinner <revengingsinner@gmail.com>
2023-03-23 14:35:47 +0200

    styles

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -33,7 +33,9 @@ export default function ContactList() {
       {visibilityContacts.map(contact => (
         <li className={styles.item} key={contact.id}>
           <span className={styles.name}>{contact.name}: </span>
-          <span className={styles.number}>{contact.number}</span>
+          <a href={`tel:${contact.number}`} className={styles.number}>
+            {contact.number}
+          </a>
           <button
             className={styles.button}
             type="button"
--- src/components/Form/ContactForm.jsx
+++ src/components/Form/ContactForm.jsx
@@ -41,6 +41,7 @@ export default function ContactForm() {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          autoComplete='true'
           placeholder="Master Yoda"
         />
       </label>
